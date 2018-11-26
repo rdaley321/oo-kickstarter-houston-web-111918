d6d35b87f62d7a4d36688a5c04d12fd9ab5e166b
Ryan Daley <rdaley321@yahoo.com>
2018-11-26 05:26:56 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,7 @@
 class Backer
-  attr_accessor :backed_projects
-  def initialize
+  attr_accessor :backed_projects, :name
+  def initialize(name)
     @backed_projects = []
+    @name = name
   end
 end
