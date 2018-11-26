f6662cd816c241fd7832a8bd9cc8269d925796fd
Ryan Daley <rdaley321@yahoo.com>
2018-11-26 05:31:01 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,12 @@
+class Project
+  attr_accessor :backers, :title
+  def initialize(title)
+    @backers = []
+    @title = title
+  end
+  
+  def add_backer(backer)
+    self.backers << backer
+    backer.backed_projects << self
+  end
+end
